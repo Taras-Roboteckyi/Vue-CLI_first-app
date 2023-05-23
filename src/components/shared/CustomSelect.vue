8bcadf924e0de4fd1e7849a2d52ea922e795aca2
Taras-Roboteckyi <rtvr@ukr.net>
2023-05-23 23:25:51 +0300

    add custom select

---
+++ src/components/shared/CustomSelect.vue
@@ -0,0 +1,33 @@
+<template>
+    <select v-on="listeners"></select>
+</template>
+
+<script>
+export default {
+    name: 'CustomSelect',
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    computed: {
+        listeners() {
+            return {
+                ...this.$listeners,
+                input: event => this.$emit('input', event.target.value)
+            }
+        },
+        formatedItems() {
+            return this.items.map(item => {
+                return typeof item === "object" ? item : {
+                    value: item, label: item
+                }
+            })
+        }
+
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
