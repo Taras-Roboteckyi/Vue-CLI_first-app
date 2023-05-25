f7ca5e9695ea49e5cc02e3f957598dad542d70fe
Taras-Roboteckyi <rtvr@ukr.net>
2023-05-25 23:27:04 +0300

    add component apartment filter form

---
+++ src/components/apartment/ApartmentFilterForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <form>
+        <CustomSelect />
+        <CustomInput />
+    </form>
+</template>
+
+<script>
+import CustomInput from '../shared/CustomInput.vue';
+import CustomSelect from '../shared/CustomSelect.vue';
+
+export default {
+    components: { CustomSelect, CustomInput }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
