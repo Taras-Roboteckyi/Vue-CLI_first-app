63333fc8064d4cedf1fae3e0a1fdda6714a9c94b
Taras-Roboteckyi <rtvr@ukr.net>
2023-06-05 23:11:45 +0300

    add component footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
+    <div class="content">
 
-    <Container>
-      <ApartmentFilterForm class="apartments-filter" @submit="filter" />
-    </Container>
-    <p v-if="!filteredApartments.length">Nothing found</p>
-    <!-- //Дивиться якщо довжина не менше 0 тоді гасить компонент// -->
+      <Container>
+        <ApartmentFilterForm class="apartments-filter" @submit="filter" />
+      </Container>
+      <Container>
+        <p v-if="!filteredApartments.length">Nothing found</p>
+        <!-- //Дивиться якщо довжина не менше 0 тоді гасить компонент// -->
 
-    <ApartmentsList v-else :items="filteredApartments">
-      <template v-slot:apartment="{ apartment }">
-        <ApartmentsItem :key="apartment.id" :descr="apartment.descr" :rating="apartment.rating" :price="apartment.price"
-          :imgSrc="apartment.imgUrl" @click.native="handleItemClick" />
-      </template>
-    </ApartmentsList>
+        <ApartmentsList v-else :items="filteredApartments">
+          <template v-slot:apartment="{ apartment }">
+            <ApartmentsItem :key="apartment.id" :descr="apartment.descr" :rating="apartment.rating"
+              :price="apartment.price" :imgSrc="apartment.imgUrl" @click.native="handleItemClick" />
+          </template>
+        </ApartmentsList>
+      </Container>
+    </div>
+    <AppFooter />
   </div>
 </template>
 
@@ -26,12 +31,13 @@ import apartments from "./components/apartment/apartments";
 import ApartmentsItem from "./components/apartment/ApartmentsItem.vue"
 import ApartmentFilterForm from "./components/apartment/ApartmentFilterForm.vue";
 import Container from "./components/shared/Container.vue";
+import AppFooter from "./components/Footer.vue"
 
 
 export default {
 
   name: "App",
-  components: { ApartmentsList, ApartmentsItem, ApartmentFilterForm, Container },
+  components: { ApartmentsList, ApartmentsItem, ApartmentFilterForm, Container, AppFooter },
   data() {
     return {
       apartments, text: '', filters: {
@@ -74,13 +80,14 @@ export default {
   font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; //Прибиває футер до низу//
 }
 
-
+.content {
+  flex-grow: 1;
+}
 
 .apartments-filter {
   margin-bottom: 40px;
