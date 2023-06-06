36562ab55c7d219c02b28f261dcc500e762dab74
Taras-Roboteckyi <rtvr@ukr.net>
2023-06-06 23:57:37 +0300

    add component Header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="header">
+        <Container>
+
+        </Container>
+    </header>
+</template>
+
+<script>
+import Container from './shared/Container.vue';
+
+export default {
+    name: 'HeaderApp',
+    components: { Container }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variables';
+
+.header {
+    background: $secondary-color;
+    padding: 16px 0;
+}
+</style>
