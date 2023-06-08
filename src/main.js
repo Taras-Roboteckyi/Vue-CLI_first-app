0741f3e846100840f1606524647139d03ba01315
Taras-Roboteckyi <rtvr@ukr.net>
2023-06-08 23:33:45 +0300

    add vue-router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./assets/scss/index.scss";
 
+import VueRouter from "vue-router";
+
 Vue.config.productionTip = false;
+Vue.use(VueRouter); //Реєструєм роутінг в нашому додатку///
 
 new Vue({
   render: (h) => h(App),
