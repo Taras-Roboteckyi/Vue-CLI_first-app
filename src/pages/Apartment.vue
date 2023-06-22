79ed021396a8e955b249e099fd9b22653bf6938a
Taras-Roboteckyi <rtvr@ukr.net>
2023-06-22 23:48:56 +0300

    add apartment inside page

--- src/pages/Apartment.vue
+++ src/pages/Apartment.vue
@@ -9,15 +9,21 @@
 
 <script>
 import Container from '../components/shared/Container'
+import apartments from "../components/apartment/apartments";
 
 export default {
     name: 'ApartmentPage',
     components: { Container },
-    ///Додаєм хук mounted///
-    mounted() {
-        console.log(this.$route.params.id),//Так можна достукатися до параметрів роута -name,id,query і т.д, а також їх передати///
-            console.log(this.$route.query.name)
+    computed: {
+        apartment() {
+            return apartments.find(apartment => apartment.id === this.$route.params.id)//Шукаєм необхідний id серед апартаментів і підставляєм його//
+        }
     }
+    ///Додаєм хук mounted///
+    /*  mounted() {
+         console.log(this.$route.params.id),//Так можна достукатися до параметрів роута -name,id,query і т.д, а також їх передати///
+             console.log(this.$route.query.name)
+     } */
 }
 </script>
 
