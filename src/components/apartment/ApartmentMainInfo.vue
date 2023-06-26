181a37e7905597876308cc98ebc60413d2c54b2a
Taras-Roboteckyi <rtvr@ukr.net>
2023-06-26 23:27:41 +0300

    add props to apartmentMainInfo

--- src/components/apartment/ApartmentMainInfo.vue
+++ src/components/apartment/ApartmentMainInfo.vue
@@ -1,12 +1,20 @@
 <template>
     <article class="apartment-main-info">
-
+        <h1 class="apartment-main-info__item"></h1>
+        <img src="" alt="" class="apartment-main-info_photo">
+        <p class="apartment-main-info_description"></p>
     </article>
 </template>
 
 <script>
 export default {
-
+    name: 'ApartmentMainInfo',
+    props: {
+        apartment: {
+            type: Object,
+            required: true
+        }
+    }
 }
 </script>
 
