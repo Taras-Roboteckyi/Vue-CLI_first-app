7ee57e7df1f3c850c3dfa25a5bb362154ffad503
Taras-Roboteckyi <rtvr@ukr.net>
2023-06-29 23:16:07 +0300

    add apartments owner info component

---
+++ src/components/apartment/ApartmentsOwner.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="apartments-owner">
+        <h2 class="apartments-owner__title">Іnformation about the owner</h2>
+        <p class="apartments-owner__info">Name: {{ owner.name }} </p>
+        <p class="apartments-owner__info">tel: {{ owner.phone }} </p>
+        <p class="apartments-owner__info">email: {{ owner.email }} </p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ApartmentsOwner',
+    props: {
+        owner: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
