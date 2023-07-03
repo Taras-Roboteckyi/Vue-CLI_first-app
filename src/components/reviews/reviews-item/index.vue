216678c3d4f5887dcefe056dfa77866b65e7062a
Taras-Roboteckyi <rtvr@ukr.net>
2023-07-03 23:21:56 +0300

    created a component ReviewsHeading №21

---
+++ src/components/reviews/reviews-item/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="reviews-item">
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ReviewsItem'
+
+}
+</script>
+
+<style lang="scss" scoped></style>
