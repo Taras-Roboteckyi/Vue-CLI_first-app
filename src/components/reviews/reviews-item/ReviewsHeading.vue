ff65401aa27d647855ce365d10c00ba449739f02
Taras-Roboteckyi <rtvr@ukr.net>
2023-07-04 23:30:52 +0300

    add skeleton to component RewiewsHeding

--- src/components/reviews/reviews-item/ReviewsHeading.vue
+++ src/components/reviews/reviews-item/ReviewsHeading.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="reviews-heading">
         <!-- avatar -->
-
+        <div class="reviews-heading__content">
+            <span class="reviews-heading__author">{{ author }}</span>
+            <Rating :rating="rating" />
+        </div>
     </div>
 </template>
 
 <script>
+import Rating from "../../StarRating.vue"
+
 export default {
+    name: 'ReviewsHeading',
+    components: { Rating },
+    props: {
+        author: {
+            type: String,
+            required: true
+        },
+        rating: {
+            type: Number,
+            default: 0
+        },
+
+    }
 
 }
 </script>
