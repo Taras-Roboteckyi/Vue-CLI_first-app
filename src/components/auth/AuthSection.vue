d96cd20e202d997cd38705dfe5e1d91186b051d4
Taras-Roboteckyi <rtvr@ukr.net>
2023-08-07 23:19:53 +0300

    add styled to AuthSection and add z-index to Header

--- src/components/auth/AuthSection.vue
+++ src/components/auth/AuthSection.vue
@@ -12,6 +12,7 @@ export default {
 
 <style lang="scss" scoped>
 .auth-section {
+    position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -20,5 +21,21 @@ export default {
     background-size: cover;
     background-position: center;
     min-height: calc(100vh - 30px);
+    z-index: 1;
+    /* Хак....щоб оверлей завжди залишався під внутрішнім контентом нашої секції */
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(15, 29, 45, 0.5);
+        z-index: -1;
+        /* Хак....щоб оверлей завжди залишався під внутрішнім контентом нашої секції */
+
+
+    }
 }
 </style>
