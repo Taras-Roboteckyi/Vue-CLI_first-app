bbf34ec26f5044ee6e544ebbff790e38e1977ab5
Taras-Roboteckyi <rtvr@ukr.net>
2023-09-14 22:48:42 +0300

    add myOrdersPage

---
+++ src/pages/MyOrders.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="my-orders-page">
+        <SectionWithHeaderSpacer>
+            <Container>
+                <section class="my-orders-page__content">
+                    <MainTitle>Orders</MainTitle>
+                </section>
+            </Container>
+        </SectionWithHeaderSpacer>
+    </main>
+</template>
+
+<script>
+import Container from '../components/shared/Container'
+import SectionWithHeaderSpacer from '@/components/shared/SectionWithHeaderSpacer.vue'
+import MainTitle from '@/components/shared/MainTitle.vue';
+
+export default {
+    name: 'MyOrdersPage',
+    components: { Container, SectionWithHeaderSpacer, MainTitle }
+}
+</script>
+
+<style lang="scss" scoped></style>
