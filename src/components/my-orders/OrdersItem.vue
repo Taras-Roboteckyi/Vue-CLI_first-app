e2b80d1c885eda625ce0127f8e4bc96f3c5fef97
Taras-Roboteckyi <rtvr@ukr.net>
2023-09-15 22:43:27 +0300

    add orders-item component

---
+++ src/components/my-orders/OrdersItem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="orders-item">
+        <img :src="order.imgUrl" alt="photo of the apartment" class="orders-item__photo">
+        <div class="orders-item__details">
+            <div class="orders-item__text">
+                <h2 class="orders-item__title">{{ order.title }}</h2>
+                <p class="orders-item__location">{{ order.location.city }}</p>
+            </div>
+            <div class="orders-item__cost">
+                <span class="orders-item__prise">{{ order.prise }}UAH</span>
+                pay per night
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OrdersItem',
+    props: {
+        order: {
+            type: Object,
+            required: true
+        }
+
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/my-orders/OrdersList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="orders-list">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OrdersList'
+}
+</script>
+
+<style lang="scss" scoped></style>
