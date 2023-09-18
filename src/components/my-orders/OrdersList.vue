a32ca0b31e869aaa6dae02fa5c718f024f630645
Taras-Roboteckyi <rtvr@ukr.net>
2023-09-18 21:42:10 +0300

    add orders list and implemented it in myOrders page

--- src/components/my-orders/OrdersList.vue
+++ src/components/my-orders/OrdersList.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="orders-list">
+        <p v-if="!items.length">There are no orders</p>
+        <template v-else>
+            <OrdersItem v-for="order in items" :key="order.id" :order="order.apartment" />
+        </template>
 
     </div>
 </template>
 
 <script>
+import OrdersItem from "./OrdersItem.vue"
+
 export default {
-    name: 'OrdersList'
+    name: 'OrdersList',
+    components: { OrdersItem },
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+
+    },
 }
 </script>
 
--- src/pages/MyOrders.vue
+++ src/pages/MyOrders.vue
@@ -4,6 +4,7 @@
             <Container>
                 <section class="my-orders-page__content">
                     <MainTitle>Orders</MainTitle>
+                    <OrdersList :items="orders" />
                 </section>
             </Container>
         </SectionWithHeaderSpacer>
@@ -14,10 +15,35 @@
 import Container from '../components/shared/Container'
 import SectionWithHeaderSpacer from '@/components/shared/SectionWithHeaderSpacer.vue'
 import MainTitle from '@/components/shared/MainTitle.vue';
+import OrdersList from '@/components/my-orders/OrdersList.vue';
+
+import { getOrders } from '@/services/order_service';
 
 export default {
     name: 'MyOrdersPage',
-    components: { Container, SectionWithHeaderSpacer, MainTitle }
+
+    components: { Container, SectionWithHeaderSpacer, MainTitle, OrdersList },
+
+    data() {
+        return {
+            orders: []
+        }
+    },
+
+    async created() {
+        try {
+            const { data } = await getOrders()
+            this.orders = data
+
+        } catch (error) {
+            this.$notify({
+                type: 'error',
+                title: 'Something went wrong',
+                text: error.message
+
+            })
+        }
+    }
 }
 </script>
 
